2d7f3f328bb90307c16b0ae029ae475bf883d2c8
INFAA <ceyhun70@live.nl>
2020-08-01 16:12:04 +0200

    wip

--- resources/views/email/orderConfirmation.blade.php
+++ resources/views/email/orderConfirmation.blade.php
@@ -4,17 +4,26 @@ Beste {{ \Illuminate\Support\Facades\Auth::user()->firstname  }} ,
 We hopen dat je het leuk vond om bij ons te shoppen! We sturen jou een mail zodra je bestelling verzonden is.
 <br><br>
 Afleveradres
+<br><br>
     {{ \Illuminate\Support\Facades\Auth::user()->firstname." ".\Illuminate\Support\Facades\Auth::user()->suffix." ".\Illuminate\Support\Facades\Auth::user()->lastname }}
-    {{ \Illuminate\Support\Facades\Auth::user()->street." ".\Illuminate\Support\Facades\Auth::user()->house_number. " ".\Illuminate\Support\Facades\Auth::user()->house_number_suffix }}
-    {{ \Illuminate\Support\Facades\Auth::user()->zipcode." ".\Illuminate\Support\Facades\Auth::user()->city }}
 <br>
+    {{ \Illuminate\Support\Facades\Auth::user()->street." ".\Illuminate\Support\Facades\Auth::user()->house_number. " ".\Illuminate\Support\Facades\Auth::user()->house_number_suffix }}
+<br>
+    {{ \Illuminate\Support\Facades\Auth::user()->zipcode." ".\Illuminate\Support\Facades\Auth::user()->city }}
+<br><br>
 Besteldatum: {{ \Carbon\Carbon::now()->format('d-M-Y') }}
 <br><br>
 
 <p><b>Artikelen</b></p>
-<img src="{{ $product->image->first_resized_image }}" alt="img" style="width: 125px; height: 200px;">
+<img src="http://127.0.0.1:8000/storage/{{ $product->image->first_resized_image }}" alt="img" style="width: 125px; height: 200px;">
 <br>
 Productnaam: {{ $product->title }}
 <br>
 Aantal: {{ $order->quantity }}
+<br>
+Prijs: {{ $product->price }}
+<br>
+<hr>
+<br>
+Betalingswijze: pp
 
